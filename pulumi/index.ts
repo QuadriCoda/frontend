8282cac14d4afbcdc3888112c17accea01527566
Dan Gurgui <dan@gurgui.com>
2023-11-21 14:07:17 +0200

    Adding ingress

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -29,3 +29,43 @@ const kustomizeConfigScrapperAdminFrontend = new k8s.yaml.ConfigGroup('kustomize
 
 // Export service name for scrapper-admin-frontend
 export const scrapperAdminFrontendServiceName = scrapperAdminFrontendChart.getResource('v1/Service', scrapperAdminFrontendName).metadata.name;
+const scrapperAdminFrontendIngress = new k8s.networking.v1.Ingress("scrapper-admin-frontend-ingress", {
+  metadata: {
+      name: "scrapper-admin-frontend-ingress",
+      annotations: {
+          "kubernetes.io/ingress.class": "nginx",
+          // Add any other necessary annotations
+      },
+  },
+  spec: {
+      rules: [
+          {
+              host: "scrapper-admin-frontend.local", // Change this to your desired hostname
+              http: {
+                  paths: [
+                      {
+                          path: "/",
+                          pathType: "Prefix",
+                          backend: {
+                              service: {
+                                  name: scrapperAdminFrontendServiceName,
+                                  port: {
+                                      number: 30000, // Update this to the correct port of your service
+                                  },
+                              },
+                          },
+                      },
+                  ],
+              },
+          },
+      ],
+      // Uncomment and configure this section if you need TLS/HTTPS
+      // tls: [{
+      //     hosts: ["scrapper-admin-frontend.local"],
+      //     secretName: "scrapper-admin-frontend-tls", // You'll need to create a TLS secret for this
+      // }],
+  },
+}, { provider: k8sProvider });
+
+// Export the Ingress hostname
+export const scrapperAdminFrontendIngressHostname = scrapperAdminFrontendIngress.metadata.name;
